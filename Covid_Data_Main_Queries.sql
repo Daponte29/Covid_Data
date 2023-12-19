4c0fce49e338e9d235503e00219f42f3b6b3af24
Nicholas Daponte <ndaponte3@gatech.edu>
2023-12-19 15:02:42 -0500

    Added to Queries

--- Covid_Data_Main_Queries.sql
+++ Covid_Data_Main_Queries.sql
@@ -1,7 +1,7 @@
 Use [Project1-Covid];
 
 
-
+--Explore 'CovidDeaths' table
 Select *
 from [Project1-Covid]..CovidDeaths
 order by 3,4;
@@ -16,7 +16,7 @@ order by 1,2;
  FROM CovidDeaths
  ORDER BY 1,2;
 
- --Percent of population who got Covid in the United States
+ --Percent of population who contracted Covid in the United States
  SELECT	location, date, total_cases, total_deaths, (total_cases/population)*100 AS PercentCcontracted
  FROM CovidDeaths
  WHERE location = 'United States'
@@ -39,15 +39,86 @@ GROUP BY population, location
 ORDER BY HighestPercentContracted DESC;
 
 -- Countries with Highest Death Count per Population
-SELECT location, MAX(total_deaths) / MAX(population) AS DeathPercentagePerPopulation
+SELECT location, (MAX(CAST(total_deaths AS int)) / MAX(population))*100 AS DeathPercentagePerPopulation
 FROM CovidDeaths
+WHERE continent IS NOT NULL --This is to make sure that only countries show and not the World locations
 GROUP BY location
-HAVING MAX(total_deaths) / MAX(population) IS NOT NULL
+HAVING MAX(CAST(total_deaths AS int)) / MAX(population) IS NOT NULL
 ORDER BY DeathPercentagePerPopulation DESC;
 
+--Countries Max Total Deaths 
+SELECT location , MAX(CAST(total_deaths AS int)) AS MaxTotalDeaths
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+Group by location
+HAVING MAX(CAST(total_deaths AS int)) IS NOT NULL
+Order by MaxTotalDeaths DESC;
+
+--Let check out Continents 
+SELECT location , MAX(CAST(total_deaths AS int)) AS MaxTotalDeaths
+FROM CovidDeaths
+WHERE continent IS NULL
+Group by location
+Order by MaxTotalDeaths DESC;
+
+--Determine Total Timeline from the CovidDeath table
+SELECT 
+    DATEDIFF(YEAR, MIN(date), MAX(date)) AS Years,
+    DATEDIFF(MONTH, MIN(date), MAX(date)) % 12 AS Months,
+    DATEDIFF(DAY, MIN(date), MAX(date)) % 30 AS Days
+FROM CovidDeaths; ---Around 1 Year and 3 Months worth of RawData
+
+---GLOBAL NUMBERS(Daily)
+SELECT date, SUM(new_cases) AS Total_Cases , SUM(CAST(new_deaths AS int)) AS Daily_Deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS DeathPercentage_Per_Day
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1,2;
+
+---GLOBAL NUMBERS(Total)
+SELECT SUM(new_cases) AS Total_Cases , SUM(CAST(new_deaths AS int)) AS Total_Deaths, SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 AS Death_Percentage
+FROM CovidDeaths
+WHERE continent IS NOT NULL
+--GROUP BY date
+ORDER BY 1,2;
+
+----LOOKING AT CovidVaccinations Table Now----------------------
+
+SELECT CD.continent, CD.location, CD.date, CD.population, CV.new_vaccinations 
+FROM CovidDeaths CD
+JOIN CovidVaccinations CV ON CD.location = CV.location AND CV.date=CD.date
+WHERE CD.continent IS NOT NULL
+ORDER BY 2,3;
+
+--Total Population VS Vaccinations as a CTE Table
+WITH PopVSVac(continent, location, date, population, new_vaccinations, Rolling_Count_Vaccinated_Total)
+AS (
+SELECT CD.continent, CD.location, CD.date, CD.population, CV.new_vaccinations, SUM(CONVERT(int, CV.new_vaccinations)) OVER(PARTITION BY CD.location ORDER BY CD.location, CD.date) AS Rolling_Count_Vaccinated_Total
+FROM CovidDeaths CD
+JOIN CovidVaccinations CV ON CD.date=CV.date AND CD.location = CV.location
+WHERE CD.continent IS NOT NULL
+--ORDER BY 2,3;
+)
+--Percent of Population Vaccinated at each date
+SELECT *, (Rolling_Count_Vaccinated_Total/population)*100 AS Percent_Population_Vaccinated
+FROM PopVSVac;
 
 
+---CREATE VIEWS for visualizations
 
+CREATE VIEW Percent_Population_Vaccinated AS
+WITH PopVSVac(continent, location, date, population, new_vaccinations, Rolling_Count_Vaccinated_Total)
+AS (
+SELECT CD.continent, CD.location, CD.date, CD.population, CV.new_vaccinations, SUM(CONVERT(int, CV.new_vaccinations)) OVER(PARTITION BY CD.location ORDER BY CD.location, CD.date) AS Rolling_Count_Vaccinated_Total
+FROM CovidDeaths CD
+JOIN CovidVaccinations CV ON CD.date=CV.date AND CD.location = CV.location
+WHERE CD.continent IS NOT NULL
+--ORDER BY 2,3;
+)
+--Percent of Population Vaccinated at each date
+SELECT *, (Rolling_Count_Vaccinated_Total/population)*100 AS Percent_Population_Vaccinated
+FROM PopVSVac;
 
-Select * from CovidDeaths;
+--Test Percent_Population_Vaccinated VIEW
+SELECT * FROM Percent_Population_Vaccinated;
 
